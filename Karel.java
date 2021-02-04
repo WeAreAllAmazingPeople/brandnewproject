ca5f72b0b69a5f9965ae4cf1c66ada7c91f3ed3a
karelVR <karel.vanroey@cegeka.com>
2021-02-04 09:41:00 +0100

    first commit for a awesome project

---
+++ Karel.java
@@ -0,0 +1,10 @@
+class Karel {
+public static void main (String[] args) {
+
+String cat = "Garfield";
+catAge = 7;
+
+System.out.println("hello world " + cat + ", I am " + catAge + " years old.");
+
+}
+}
